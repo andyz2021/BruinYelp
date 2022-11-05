7543efc982c29c74efdd2e60ba57669089dd96b8
Andy <andyhz822@gmail.com>
2022-11-04 18:57:23 -0700

    az: Added basic functions for backend using Node and Express Start the server in the backend with "node index.js" Might need to install some stuff

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');//Instance of express framework
+const app = express();
+const cors = require("cors");
+const PORT = 3001;
+app.use(express.json());
+app.use(cors());
+app.get('/api', (req, res) => {
+res.json({message:'hello world'})
+})
+
+app.listen(PORT, () => {
+    console.log("Server running");
+})
+
