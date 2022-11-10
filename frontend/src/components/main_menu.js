27122c0a81eb2cd3915b5005cfbf08492938f6dc
Andy <andyhz822@gmail.com>
2022-11-10 15:42:46 -0800

    AZ: Added UCLA dining hall menu to main menu page

--- frontend/src/components/main_menu.js
+++ frontend/src/components/main_menu.js
@@ -2,6 +2,19 @@ import * as React from "react";
 
 export default function Main_Menu() {
     return(
-        <h2 style = {{color: 'black', display: "flex", justifyContent: "center"}}>Menu of all the food(we'll put it in later)</h2>
+        <React.Fragment>
+            <h2 style = {{color: 'black', display: "flex", justifyContent: "center"}}>Menus</h2>
+            <div style = {{  position: "relative",
+                overflow: "hidden",
+                justifyContent: "center",
+                display: "flex",
+                alignItems: "center",}}>
+                <iframe scrolling = "no" src={"https://menu.dining.ucla.edu/Menus"} style = {{
+                    width: "1300px",
+                    display: "flex",
+                    alignItems: "center",
+                    height: "1300px",}}></iframe>
+            </div>
+        </React.Fragment>
     )
 }
