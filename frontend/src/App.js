0654c310caeee718eccdc999c2ab914260405b53
Julie Lam <julielam@Julies-MacBook-Pro.local>
2022-11-29 21:29:58 -0800

    Remove thematic break

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,6 @@ function App () {
     return (
         <>
             <h1 className = "title" style={{color: 'black', display: "flex", margin: "20px", justifyContent: "center", fontFamily: "Arial", fontWeight: "bold"}}>BruinYelp</h1>
-            <hr/>
             <AuthProvider>
                 <Navigation/>
             </AuthProvider>
